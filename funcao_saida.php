fd1b93beb9d830ab4b0f9cbd0cc81b5e64540fe9
Renan Guilherme <renangc.rzk@gmail.com>
2020-10-16 21:35:19 -0300

    Funções de saída

---
+++ funcao_saida.php
@@ -0,0 +1,18 @@
+<?php
+    echo "Função de saída echo: \n";
+    echo "hello world" . PHP_EOL; // . operador concatenação
+    echo "mensagem legal";
+?>
+    <?= "hello world" //flag echo ?> 
+
+<?php
+    echo 69 . PHP_EOL;      // inteiro
+    echo 5.344 . PHP_EOL;   // float
+?>
+
+<?php
+    echo "Função de saída var_dump: \n";
+    var_dump("Olá marilene") . PHP_EOL; // Constante End of Line EOL
+    var_dump(1234);
+    var_dump(new stdClass());
+?>
