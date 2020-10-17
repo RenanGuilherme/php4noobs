3971ed801b8f82d25dc155fd2ea850410d46e1fb
Renan Guilherme <renangc.rzk@gmail.com>
2020-10-17 11:29:31 -0300

    Concatenação strings

---
+++ concatenacao_strings.php
@@ -0,0 +1,21 @@
+<?php
+// Concatenação de Strings
+
+    $nome = 'Vilma Morsheingfjsdkcx' . ', de qual cidade?';
+    
+    echo $nome;
+
+// Concatenação de forma abreviada .=
+    echo PHP_EOL;
+    $nome = 'Vilma Morsheingfjsdkcx';
+    $nome .= ', de qua cidade?';
+    
+    echo $nome;
+
+// Concatenação de variáveis
+    $nome = 'Vilma Morsheingfjsdkcx';
+    $cidade = ' São Domingos';
+    
+    echo PHP_EOL;
+    echo $nome . $cidade;
+?>
