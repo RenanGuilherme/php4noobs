46e524d9bf955d879a58c294154e14d3669f9619
Renan Guilherme <renangc.rzk@gmail.com>
2020-10-16 21:35:56 -0300

    Variáveis

---
+++ variaveis.php
@@ -0,0 +1,17 @@
+<?php
+/** Variáveis no php são case-sensitivie. 
+* Devem começar com underscore ou letra minúsula.
+*/
+    $_He4rt = 'Eu ';
+    $he4rt = 'to ';
+    $he4rT = 'com ';
+    $_HE4RT = 'FOME';
+    echo $_He4rt . $he4rt . $he4rT . $_HE4RT . "\n";
+
+?>
+
+<?php
+// Definindo constantes.
+
+define("HAYLEY", "https://twitter.com/yelyahwilliams");
+    echo "Follow de queen " . HAYLEY . PHP_EOL;
