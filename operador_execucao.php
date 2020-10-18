63a4dc59cbb22c22c3c934bdf249b1f679746ff4
Renan Guilherme <renangc.rzk@gmail.com>
2020-10-18 11:39:37 -0300

    Operador de execução

---
+++ operador_execucao.php
@@ -0,0 +1,11 @@
+<?php
+    // Operador de execução
+
+    // Os backticks ou backquotes (``) executam o conteúdo
+    // como um comando shell e são equivalentes a shell_exec()
+    // exec() e system(), todos são capazes de executar
+    // comando no nível do shell.
+
+    $output = `ls -al`;
+    echo "<pre>$output</pre>";
+?>
